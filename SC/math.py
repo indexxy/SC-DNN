c338256e03bac88ee3b729b40c9f39b06bc65bd1
Arslan <rslnkrmt2552@gmail.com>
2020-05-18 13:52:42 +0300

    Improve code's pythonicity (LOL)

--- SC/math.py
+++ SC/math.py
@@ -5,7 +5,7 @@ from SC.stochastic import SCNumber, random_stream, bpe_encode
 
 
 @vectorize(['boolean(boolean, boolean)'])
-def mul(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+def mul_sc(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     # return np.logical_not(np.logical_xor(x, y))
     return ~(x ^ y)
 
@@ -15,7 +15,7 @@ def scaled_add(x, y, scale=0.5):
     assert len(x) == len(y)
     n = len(x)
     selection = SCNumber(scale, precision=n)
-    result = np.ndarray(n, dtype=np.bool)
+    result = np.empty(n, dtype=np.bool)
     for i in range(n):
         if selection[i]:
             result[i] = x[i]
@@ -35,7 +35,7 @@ def counter_add(x, y):
     return SCNumber(2 * (count - n) / n, precision=n)
 
 
-def sum(x: np.ndarray, conc=1):
+def sum_sc(x: np.ndarray, conc=1):
     # x is 2D numpy array holding n bit-streams
     # returns 1D numpy array as one bit-stream
     assert len(x.shape) == 2
@@ -43,7 +43,7 @@ def sum(x: np.ndarray, conc=1):
     count = np.count_nonzero(x)
     # decimal_value = (count - (len(x)*n - count) ) / n
     decimal_value = (2 * count - x.size) / n
-    result = np.ndarray(conc * n, dtype=np.bool)
+    result = np.empty(conc * n, dtype=np.bool)
     i = 0
     while decimal_value > 1 and conc != 1:
         result[i:i + n] = 1
@@ -67,17 +67,17 @@ def sum(x: np.ndarray, conc=1):
     return result
 
 
-def dot(x: np.ndarray, y: np.ndarray, conc=1):
+def dot_sc(x: np.ndarray, y: np.ndarray, conc=1):
     # x , y are 3D numpy arrays holding n*m bit-streams
     # returns 3D numpy array containing bit-streams representing the result of x dot y
     assert len(x.shape) == 3 and len(y.shape) == 3
     assert x.shape[2] == y.shape[2]  # checking precisions
     assert x.shape[1] == y.shape[0]  # (i,j) . (k,z) --> k=j
 
-    result = np.ndarray((x.shape[0], y.shape[1], x.shape[2] * conc), dtype=np.bool)
+    result = np.empty((x.shape[0], y.shape[1], x.shape[2] * conc), dtype=np.bool)
     y_t = y.transpose(1, 0, 2)
     for i in range(x.shape[0]):
         for j in range(y.shape[1]):
-            result[i][j] = sum(mul(x[i], y_t[j]), conc=conc)
+            result[i][j] = sum_sc(mul_sc(x[i], y_t[j]), conc=conc)
 
     return result
