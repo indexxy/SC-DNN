559ff47f1abca48a5385601a295f6fe3368b1c3c
Majd <majd.kharrat@gmail.com>
2020-05-23 02:32:56 +0300

    Pass scaling factor as an argument

--- SC/math.py
+++ SC/math.py
@@ -35,7 +35,7 @@ def counter_add(x, y):
     return SCNumber(2 * (count - n) / n, precision=n)
 
 
-def sum_sc(x: np.ndarray, conc=1):
+def sum_sc(x: np.ndarray, scale=1):
     # x is 2D numpy array holding n bit-streams
     # returns 1D numpy array as one bit-stream
     assert len(x.shape) == 2
@@ -43,41 +43,42 @@ def sum_sc(x: np.ndarray, conc=1):
     count = np.count_nonzero(x)
     # decimal_value = (count - (len(x)*n - count) ) / n
     decimal_value = (2 * count - x.size) / n
-    result = np.empty(conc * n, dtype=np.bool)
+    result = np.empty(scale * n, dtype=np.bool)
     i = 0
-    while decimal_value > 1 and conc != 1:
+    while decimal_value > 1 and scale != 0:
         result[i:i + n] = 1
         decimal_value -= 1
-        conc -= 1
+        scale -= 1
         i += n
 
-    while decimal_value < -1 and conc != 1:
+    while decimal_value < -1 and scale != 0:
         result[i:i + n] = 0
         decimal_value += 1
-        conc -= 1
+        scale -= 1
         i += n
 
-    if -1 <= decimal_value <= 1:
+    if -1 <= decimal_value <= 1 and scale != 0:
         # The decimal value is already quantized,  No need to quantize it again
         result[i:i + n] = random_stream(n, int(bpe_encode(decimal_value) * n))
-        if conc != 1:
-            i += n
+        i += n
+        if i+n <= result.size:
             result[i:i + n] = random_stream(n, n // 2)
+
 
     return result
 
 
-def dot_sc(x: np.ndarray, y: np.ndarray, conc=1):
+def dot_sc(x: np.ndarray, y: np.ndarray, scale=1):
     # x , y are 3D numpy arrays holding n*m bit-streams
     # returns 3D numpy array containing bit-streams representing the result of x dot y
     assert len(x.shape) == 3 and len(y.shape) == 3
     assert x.shape[2] == y.shape[2]  # checking precisions
     assert x.shape[1] == y.shape[0]  # (i,j) . (k,z) --> k=j
 
-    result = np.empty((x.shape[0], y.shape[1], x.shape[2] * conc), dtype=np.bool)
+    result = np.empty((x.shape[0], y.shape[1], x.shape[2] * scale), dtype=np.bool)
     y_t = y.transpose(1, 0, 2)
     for i in range(x.shape[0]):
         for j in range(y.shape[1]):
-            result[i][j] = sum_sc(mul_sc(x[i], y_t[j]), conc=conc)
+            result[i][j] = sum_sc(mul_sc(x[i], y_t[j]), scale=scale)
 
     return result
